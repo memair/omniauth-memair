8bbb7b6774694fc4a9bb3dfa38dd961fe12b101b
gregology <greg@gho.st>
2018-10-31 14:55:52 -0400

    appending default scope

--- lib/omniauth/strategies/memair.rb
+++ lib/omniauth/strategies/memair.rb
@@ -35,7 +35,7 @@ module OmniAuth
           end
 
           params[:redirect_uri] = options[:redirect_uri] unless options[:redirect_uri].nil?
-          params[:scope] ||= DEFAULT_SCOPE
+          params[:scope] = params[:scope]&.include?(DEFAULT_SCOPE) ? params[:scope] : params[:scope] + ' ' + DEFAULT_SCOPE
         end
       end
 
